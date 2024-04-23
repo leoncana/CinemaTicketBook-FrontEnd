dc6ca92206e8a923e1762ce591c9b04f1b9b753d
leoncana <leoncana01@hotmail.com>
2024-04-23 19:05:46 +0200

    created sign in/changed the code between sign in and sign up

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -7,28 +7,19 @@ import '../auth.scss';
 import Link from 'next/link';
 import { toast } from 'react-toastify';
 import logo from '@/assets/logo.png';
-
-
+// import { getCookie , setCookie} from 'cookies-next';
+// Define an interface for the form data
 interface FormData {
-    name: string;
     email: string;
     password: string;
-    confirmPassword: string;
-    city: string;
 }
 
-export default function Signup() {
+export default function Signin() {
     const [formData, setFormData] = useState<FormData>({
-        name: '',
         email: '',
         password: '',
-        confirmPassword: '',
-        city: ''
     });
-
     const [errors, setErrors] = useState<Record<string, string>>({});
-
-
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setFormData({
@@ -39,11 +30,7 @@ export default function Signup() {
 
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-
         e.preventDefault();
-
-        console.log(formData)
-        setErrors({})
 
         const validationErrors: Record<string, string> = {};
         if (!formData.email) {
@@ -52,44 +39,36 @@ export default function Signup() {
         if (!formData.password) {
             validationErrors.password = 'Password is required';
         }
-        if (formData.password !== formData.confirmPassword) {
-            validationErrors.confirmPassword = 'Passwords do not match';
-        }
-        if (!formData.city) {
-            validationErrors.city = 'City is required';
-        }
+
         if (Object.keys(validationErrors).length > 0) {
             setErrors(validationErrors);
             return;
         }
 
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/register`, {
+console.log(process.env.NEXT_PUBLIC_BACKEND_API)
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(formData),
+            credentials: 'include'
         })
             .then((res) => {
                 return res.json();
             })
-            .then((response) => {
+            .then(async (response) => {
                 if (response.ok) {
                     toast(response.message, {
                         type: 'success',
                         position: 'top-right',
                         autoClose: 2000
                     })
-                    window.location.href = '/auth/signin'
-                    setFormData(
-                        {
-                            name: '',
-                            email: '',
-                            password: '',
-                            confirmPassword: '',
-                            city: ''
-                        }
-                    )
+                    // await setCookie('authToken', response.data.authToken)
+                    // await setCookie('refreshToken', response.data.refreshToken)
+                    // const authToken = await getCookie('authToken');
+                    // console.log('My Cookie Value:', authToken);
+                    checkLogin()
                 } else {
                     toast(response.message, {
                         type: 'error',
@@ -105,9 +84,54 @@ export default function Signup() {
                     autoClose: 2000
                 });
             })
-    }
+    };
+
+    const checkLogin = async () => {
+        // let authToken = await getCookie('authToken')
+        // let refreshToken = await getCookie('refreshToken')
+
+        // console.log(authToken, refreshToken)
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/checklogin`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        })
+            .then((res) => {
+                return res.json();
+            })
+            .then((response) => {
+                console.log(response)
+
+
+
+                if (response.ok) {
+                    // toast(response.message, {
+                    //     type: 'success',
+                    //     position: 'top-right',
+                    //     autoClose: 2000
+                    // })
+
+                    window.location.href = "/"
+
+
+                } else {
+                    // toast(response.message, {
+                    //     type: 'error',
+                    //     position: 'top-right',
+                    //     autoClose: 2000
+                    // });
+                }
+            })
+            .catch((error) => {
+                window.location.href = "/"
+            })
+    };
+
     return (
         <div className='authout'>
+
             <div className='authin'>
                 <div className="left">
                     <Image src={logo} alt="" className='img' />
@@ -120,17 +144,6 @@ export default function Signup() {
                         }}
                         onSubmit={handleSubmit}
                     >
-                        <div className="forminput_cont">
-                            <label>Name</label>
-                            <input
-                                type="text"
-                                placeholder="Enter Your Name"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                            />
-                            {errors.name && <span className="formerror">{errors.name}</span>}
-                        </div>
                         <div className="forminput_cont">
                             <label>Email</label>
                             <input
@@ -155,40 +168,14 @@ export default function Signup() {
                                 <span className="formerror">{errors.password}</span>
                             )}
                         </div>
-                        <div className="forminput_cont">
-                            <label>Confirm Password</label>
-                            <input
-                                type="password"
-                                placeholder="Confirm Your Password"
-                                name="confirmPassword"
-                                value={formData.confirmPassword}
-                                onChange={handleChange}
-                            />
-                            {errors.confirmPassword && (
-                                <span className="formerror">{errors.confirmPassword}</span>
-                            )}
-                        </div>
 
-                        <div className="forminput_cont">
-                            <label>City</label>
-                            <input
-                                type="text"
-                                placeholder="Enter City"
-                                name="city"
-                                value={formData.city}
-                                onChange={handleChange}
-                            />
-                            <datalist id="city-list"> 
-                                <option value="Prishtine" /> 
-                                <option value="Prizren" /> 
-                            </datalist>
-                            {errors.city && (
-                                <span className="formerror">{errors.city}</span>
-                            )}
-                        </div>
+                        <button type="submit" className="main_button">
+                            Login
+                        </button>
 
-                        <button type="submit" className="main_button">Register</button>
-                        <p className='authlink'>Already have an account? <Link href="/auth/signin">login</Link></p>
+                        <p className="authlink">
+                            Don't have an account? <Link href="/auth/signup">Register</Link>
+                        </p>
                     </form>
                 </div>
 
